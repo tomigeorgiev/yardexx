137b47137dd6573e2b63ff41668867f660883d80
tomigeorgiev <53129276+tomigeorgiev@users.noreply.github.com>
2021-08-10 16:56:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Coming Soon...
-        </p>
+        </p><hr/>
         <p> Developed by Yardex </p>
       </header>
     </div>
